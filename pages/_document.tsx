b8be2b49072bdaf0f10afdc76c2ab24d2ba70bd6
Sainath Kommagoni <jsreactdevsai@gmail.com>
2022-11-28 14:53:07 +0530

    merge commit

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,23 +1,15 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
-export interface ILink {
-	crossOrigin: Boolean;
-}
-
 export default function Document() {
 	return (
 		<Html>
 			<Head>
 				<link rel="preconnect" href="https://fonts.googleapis.com" />
-<<<<<<< HEAD
 				<link
 					rel="preconnect"
 					href="https://fonts.gstatic.com"
 					crossOrigin="true"
 				/>
-=======
-				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
->>>>>>> 9c67a747da1ccaf65478ed6c671c659afea648d2
 				<link
 					href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
 					rel="stylesheet"
