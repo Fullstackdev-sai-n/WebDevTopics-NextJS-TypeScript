76be5a7c2a33de5879bf90a78b12455f1acb8653
Sainath Kommagoni <jsreactdevsai@gmail.com>
2022-12-08 00:21:46 +0530

    added homepage 3d objects and usemediaquery hook for rendering elements based on viewports

--- components/test.tsx
+++ components/test.tsx
@@ -2,9 +2,7 @@ import React from "react";
 import { useGLTF } from "@react-three/drei";
 
 export default function Model() {
-	const model = useGLTF("http://localhost:3000/stackTitle.glb");
-
-	console.log(model.animations);
+	const model = useGLTF("http://localhost:3000/title.glb");
 
 	return <primitive object={model.scene} />;
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,17 @@
 import Image from "next/image";
-import Link from "next/link";
 import homeBg from "../public/homeBg.jpg";
 import NextHead from "../utils/NextHead";
 import { lazy, Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
+import { Html, OrbitControls } from "@react-three/drei";
+import Link from "next/link";
+import SpaceDust from "../components/Particles";
 
 export default function Home() {
-	const ModelComponent = lazy(() => import("../components/test"));
+	const TeslaModel = lazy(() => import("../components/teslaBot"));
+	const MainTitleModel = lazy(() => import("../components/Title"));
+	const DesktopModel = lazy(() => import("../components/Desktop"));
+
 	return (
 		<>
 			<NextHead
@@ -23,56 +28,21 @@ export default function Home() {
 					placeholder="blur"
 					src={homeBg}
 					alt=""
-				/>
-				<Suspense fallback={"loading"}>
-					<Canvas camera={{ position: [1, 1, 1] }}>
-						<ModelComponent />
-						<color attach="background" args={["hotpink"]} />
-					</Canvas>
-				</Suspense>
+				/>{" "}
+				<Canvas>
+					<OrbitControls />
+					<directionalLight intensity={0.5} />
+					<ambientLight intensity={0.4} />
+					<MainTitleModel />
+				</Canvas>
+				<div className="absolute top-0 bg-green-400 opacity-60 h-full w-full">
+					<Canvas>
+						<OrbitControls />
+						<directionalLight intensity={0.5} />
+						<ambientLight intensity={0.4} />
 
-				<div className="absolute top-0 bg-green-400 opacity-60 h-full w-full"></div>
-				<div className="grid absolute mx-auto right-0 left-0 top-24 px-6 sm:px-24 pb-10">
-					<h1 className="text-3xl sm:text-6xl text-black home-title-animation py-6">
-						&lt;STACK WEB DEV&gt;
-					</h1>
-					{/* <div className="grid bg-slate-100/20 rounded-lg p-2 place-items-center text-black shadow-black">
-						<ul className="">
-							<Link href="/typescript-errors">
-								<li className="py-4 px-4 text-2xl hover:text-3xl">
-									TypeScipt Errors List
-								</li>
-							</Link>
-							<Link href="/css">
-								<li className="py-4 text-2xl hover:text-3xl">
-									CSS Positioning Elements
-								</li>
-							</Link>
-							<Link href="/javascript">
-								<li className="py-4 text-2xl hover:text-3xl">
-									JavaScript addEventListener()
-								</li>
-							</Link>
-							<Link href="/machinelearning">
-								<li className="py-4 text-2xl hover:text-3xl">
-									What is Machine Learning?
-								</li>
-							</Link>
-							<Link href="/next13overview">
-								<li className="py-4 text-2xl hover:text-3xl">
-									NextJS 13 Release Updates
-								</li>
-							</Link>
-							<Link href="/webcomponents">
-								<li className="py-4 text-2xl hover:text-3xl">
-									How to implement and use WebComponents
-								</li>
-							</Link>
-							<li></li>
-							<li></li>
-							<li></li>
-						</ul>
-					</div> */}
+						<DesktopModel />
+					</Canvas>
 				</div>
 			</section>
 		</>
